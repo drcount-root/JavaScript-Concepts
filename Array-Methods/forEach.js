684aea8e3d207a75b5ab06b8797d521e598b6887
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-01 14:20:10 +0530

    .

---
+++ Array-Methods/forEach.js
@@ -0,0 +1,71 @@
+// The forEach() method executes a provided function once for each array element.
+
+// forEach((item, index, array) => {}, thisArg)
+// return value => undefined
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const res = movements.forEach((mov, i) => {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
+  }
+});
+
+console.log(res);
+
+//
+//
+//
+
+class Counter {
+  constructor() {
+    this.sum = 0;
+    this.count = 0;
+  }
+  add(array) {
+    // Only function expressions will have its own this binding
+    array.forEach(function countEntry(entry) {
+      this.sum += entry;
+      ++this.count;
+    }, this);
+  }
+}
+
+const obj = new Counter();
+obj.add([2, 5, 9]);
+console.log(obj.count); // 3
+console.log(obj.sum); // 16
+
+//
+//
+//
+
+// forEach With Maps
+
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//
+//
+//
+
+// forEach With Sets
+
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+// set only store unique values
--- Array-Methods/map.js
+++ Array-Methods/map.js
@@ -1,1 +1,29 @@
-/
+// The map Method
+// The map() method returns a new array populated with the results of calling a provided function on every element in the calling array.
+
+// it doesn't mutate the original array
+
+// map((item, index, array) => { /* … */ }, thisArg)
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+
+console.log(movementsUSD);
+
+//
+//
+//
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
