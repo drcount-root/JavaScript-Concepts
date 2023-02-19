8094ae89dbce0f6601aca59e9e59703c41dbabb2
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-19 23:29:36 +0530

    .

---
+++ String-Methods/String-Methods/toString.js
@@ -0,0 +1,14 @@
+// The toString() method returns a string representing the specified string value.
+
+const stringObj = new String("foo");
+
+console.log(stringObj);
+// String { "foo" }
+
+console.log(stringObj.toString());
+// "foo"
+
+const x = new String("Hello world");
+
+console.log(x.toString()); 
+// "Hello world"
